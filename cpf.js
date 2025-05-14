093a0d6d0ed682743f4ea046fcef1702e469d435
RafaelaMichelin <Rafaelamichelin@gmail.com>
2025-05-13 22:14:23 -0300

    implementando ação para mostrar a mensagem caso as condições sejam efetuadas

--- cpf.js
+++ cpf.js
@@ -3,7 +3,7 @@ function validarCPF(cpf){
 
 
     //condição
-    if(cpf.lenght !== 11 || /^(\d)\1+$/.test(cpf)){   //expressao para ver se esta sendo digitado varios numeros iguais
+    if(cpf.length!==11 || /^(\d)\1+$/.test(cpf)){   //expressao para ver se esta sendo digitado varios numeros iguais
   
         return false;
     }
@@ -18,7 +18,7 @@ function validarCPF(cpf){
         soma += parseInt(cpf.substring( i-1, i)) * (11-i);
     }
 
-    resto = ((soma*10) % 11);                 //mod % é o resto da divisão
+    resto = (soma*10) % 11;                 //mod % é o resto da divisão
     
 
     if(resto === 10 || resto === 11)
@@ -51,9 +51,29 @@ function validarCPF(cpf){
  }
 
  //válido: retorna TRUE
- 
+
  return true;
 
+}
 
+
+//
+document.getElementById("cpfform").addEventListener("submit", function(e){
+//evita atualização da pg
+e.preventDefault();
+const cpfInput = document.getElementById("cpf").value;
+
+const messageDiv = document.getElementById("message");
+
+if(validarCPF(cpfInput)){
+    messageDiv.textContent = "CPF Válido";
+    
+    messageDiv.className = "message sucess";   //ativa oque está no css para ficar com cor verde
+
+} else{
+    messageDiv.textContent = "CPF inválido";
+    messageDiv.className = "message error";
 
 }
+messageDiv.style.display = "block";
+});
